fd3c84689d52b5a05b4d29cac8f2ecb6e0d6e11d
Andrey Paramonov <github@ndpar.com>
2020-04-18 11:38:29 -0400

    Proper logging

--- src/main/kotlin/com/ndpar/redis/rendezvous/broker.kt
+++ src/main/kotlin/com/ndpar/redis/rendezvous/broker.kt
@@ -6,6 +6,8 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeoutOrNull
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.connection.Message
 import org.springframework.data.redis.connection.MessageListener
@@ -18,27 +20,29 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 private const val TIMEOUT = 20_000L
 
 interface ChannelSubscription {
-    fun subscribe(key: String, store: Channel<String>)
+    fun subscribe(key: String, channel: Channel<String>)
     fun unsubscribe(key: String)
 }
 
 @Component
 class RedisTopicListener : MessageListener, ChannelSubscription {
+    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     private val subscriptions: ConcurrentMap<String, Channel<String>> = ConcurrentHashMap()
 
-    override fun subscribe(key: String, store: Channel<String>) {
-        println("Subscribing: $key=$store")
-        subscriptions.putIfAbsent(key, store)
+    override fun subscribe(key: String, channel: Channel<String>) {
+        logger.debug("Subscribing to routing key={}", key)
+        subscriptions.putIfAbsent(key, channel)
     }
 
     override fun unsubscribe(key: String) {
-        println("Unsubscribing: $key")
+        logger.debug("Unsubscribing from routing key={}", key)
         subscriptions.remove(key)?.close()
     }
 
     override fun onMessage(message: Message, pattern: ByteArray?) {
         val key = String(message.channel)
-        println("On message: $key=$message")
+        logger.debug("On message: {}={}", key, message)
         notify(key, message.toString())
     }
 
@@ -62,6 +66,7 @@ class RedisMessageBroker(
     @Autowired private val template: StringRedisTemplate,
     @Autowired private val listener: ChannelSubscription
 ) : MessageBroker {
+    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
 
     override fun leftSend(key: String, value: String) = send(key.left, value)
     override fun leftReceive(key: String): String? = receive(key.right)
@@ -71,7 +76,9 @@ class RedisMessageBroker(
 
     private fun send(key: String, value: String) =
         with(template) {
+            logger.debug("Setting k/v: {}={}", key, value)
             opsForValue().set(key, value, TIMEOUT, MILLISECONDS)
+            logger.debug("Sending message: channel={}, message={}", key, value)
             convertAndSend(key, value)
         }
 
@@ -82,6 +89,7 @@ class RedisMessageBroker(
         val rendezvous = Channel<String>()
         listener.subscribe(key, rendezvous)
         val result = withTimeoutOrNull(TIMEOUT) {
+            logger.debug("Waiting for message on channel={}", key)
             rendezvous.receive()
         }
         listener.unsubscribe(key)
